a88effaa24d67b80e2e2eb6f086048650c6ae4a8
MrEmmanuel <sejesoo@gmail.com>
2021-09-16 23:56:48 +0200

    Added test for Add up to two numbers

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+
+    @Test
+    public void AddTwoNumbers() {
+        assertEquals(Calculator.add(""), 0, "Should return 0");
+        assertEquals(Calculator.add("1"), 1, "Should return 1");
+    }
 }
