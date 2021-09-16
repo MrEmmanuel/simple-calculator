4f85e48763574bb145bb2e54a208dc3fcde7266c
MrEmmanuel <sejesoo@gmail.com>
2021-09-17 00:00:01 +0200

    Added test for Add with different delimiters

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -20,4 +20,11 @@ public class CalculatorTest {
         assertEquals(6, Calculator.add("1\n2,3"));
     }
 
+    @Test
+    public void AddDifferentDelimiters() {
+        assertEquals(3, Calculator.add("//;\n1;2"));
+        assertEquals(3, Calculator.add("//4\n142"));
+    }
+
+
 }
