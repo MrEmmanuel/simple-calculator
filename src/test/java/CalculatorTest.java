76248352406e0c5b28950bfd8daab5e465f7b345
MrEmmanuel <sejesoo@gmail.com>
2021-09-16 23:57:46 +0200

    Added test for Add with multiple numbers

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,4 +8,10 @@ public class CalculatorTest {
         assertEquals(Calculator.add(""), 0, "Should return 0");
         assertEquals(Calculator.add("1"), 1, "Should return 1");
     }
+    
+    @Test
+    public void AddMultipleNumbers() {
+        assertEquals(Calculator.add("1,2,3,4"), 10, "Should return 10");
+
+    }
 }
