741e35e199bf6d6a9eda47af8fcf11df406a6e64
MrEmmanuel <sejesoo@gmail.com>
2021-11-19 18:48:25 +0200

    Made some few tweaks!

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,37 +2,40 @@ public class Calculator {
     public static void main(String[] args){
 
         System.out.println(Calculator.add("1,2,3,4"));
-        System.out.println(Calculator.add("//4\n142"));
-        System.out.println(Calculator.add("-1,-2,3,4"));
-        System.out.println(Calculator.add("//***\n1***2***3"));
+        System.out.println(Calculator.add("//[(-_-')][%]\n1(-_-')2%3"));
         System.out.println(Calculator.add("//[abc][777][:(]\n1abc27773:(1"));
 
     }
-
-
     public static int add(String str) {
         String delimiter = ",\n";
         if (str.startsWith("//")) {
             delimiter += str.substring(2, str.indexOf("\n"));
             str = str.substring(str.indexOf("\n"));
         }
-        return add(str, "[" + delimiter + "]");
+        return add(str, delimiter );
     }
-
-    static int add(String nums, String delimiter) {
-        String[] arr = nums.split("[" + delimiter + "]");
+    public static int add(String nums, String delimiter){
+        char specialCharacter = '#';
+        for(int i=0; i<delimiter.length(); i++){
+            if((delimiter.charAt(i) == '[') || (delimiter.charAt(i) == ']')){
+                continue;
+            }else{
+                nums = nums.replace(delimiter.charAt(i), specialCharacter);
+            }
+        }
+        String[] arr = nums.split("#");
         StringBuilder negativeNumbers = new StringBuilder();
         int sum = 0;
         try {
-            for (String numberIndex : arr) {
-                if (!numberIndex.trim().isEmpty() && (Character.isDigit(numberIndex.charAt(0)) || numberIndex.charAt(0) == '-')) {
+            for (int i=0; i< arr.length; i++) {
+                if (!arr[i].trim().isEmpty() && (Character.isDigit(arr[i].charAt(0)) || arr[i].charAt(0) == '-')) {
                     if (!Character.isDigit(nums.charAt(nums.length() - 1))) {
                         throw new IllegalArgumentException("ERROR: invalid input");
                     }
-                    if (Integer.parseInt(numberIndex.trim()) < 0) {
-                        negativeNumbers.append(numberIndex.trim()).append(" ");
-                    } else if (Integer.parseInt(numberIndex.trim()) < 1000) {
-                        sum += Integer.parseInt(numberIndex.trim());
+                    if (Integer.parseInt(arr[i].trim()) < 0) {
+                        negativeNumbers.append(arr[i].trim()).append(" ");
+                    } else if (Integer.parseInt(arr[i].trim()) < 1000) {
+                        sum += Integer.parseInt(arr[i].trim());
                     }
                 }
             }
